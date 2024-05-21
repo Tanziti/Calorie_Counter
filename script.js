1cd71e1a8d711a3f36b47b4484d0d5825822054c
Zi Tan <tanzitian3@gmail.com>
2024-05-21 14:32:36 -0400

    addEmtry entrynumber

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function isInvalidInput(str) {
 console.log(isInvalidInput("1e3"))
 
 function addEntry() {
-    const targetId = '#' + entryDropdown.value;
-    const targetInputContainer = document.querySelector(targetId + ' .input-container')
-    
-}
+    const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+    const entryNumber = targetInputContainer.querySelectorAll()
+  }
+  
