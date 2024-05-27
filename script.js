a50d1be63efde36dba331c8b58ef071c4f740a86
Zi Tan <tanzitian3@gmail.com>
2024-05-26 20:20:58 -0400

    addentry2

--- script.js
+++ script.js
@@ -23,5 +23,22 @@ console.log(isInvalidInput("1e3"))
 
 function addEntry() {
     const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
-    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+    const entryCount = targetInputContainer.querySelectorAll('input[type="text"]').length;
+    
+    console.log(entryCount); // This will log the number of text input elements within the specified container
+  
+    // Declare a new HTMLString variable and assign it an empty template literal string
+    let HTMLString = ``;
+  
+    // Here, you can build your dynamic HTML string based on the entry count or other logic
+    HTMLString += `
+      <div class="entry">
+        <input type="text" name="entryName${entryCount + 1}" placeholder="Entry Name">
+        <input type="number" name="entryCalories${entryCount + 1}" placeholder="Calories">
+      </div>
+    `;
+  
+    // Append the new HTML string to the target input container
+    targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
   }
+  
