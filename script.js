7c6296c484f41d47c44e38326f8fd7bea6ec7e78
Zi Tan <tanzitian3@gmail.com>
2024-09-22 13:21:38 -0400

    currval plus calories

--- script.js
+++ script.js
@@ -47,6 +47,9 @@ function getCaloriesFromInputs(list) {
 
     if (invalidInputMatch) {
       alert(`Invalid Input: ${invalidInputMatch[0]}`);
+      isError = true
+      return null
     }
+    calories += Number(currVal)
   }
 }
